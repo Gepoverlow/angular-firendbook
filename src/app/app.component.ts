2d9c88780ff4056f6ea1e68a273c3cf770aad078
lucas <gepeoverload@gmail.com>
2022-10-08 18:51:24 +0200

    added on component mount logic to fetch all posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from './service/post.service';
+import { Post } from './model/Post';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-friendbook';
+export class AppComponent implements OnInit {
+  private _posts: Post[];
+
+  constructor(private postService: PostService) {
+    this._posts = [];
+  }
+
+  ngOnInit() {
+    this.getPosts();
+  }
+
+  private getPosts() {
+    console.log('Retrieving posts...');
+
+    this.postService.getPosts().subscribe({
+      next: (response: Post[]) => {
+        console.log(response);
+        this._posts = response;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+
+  public get posts() {
+    return this._posts;
+  }
+
+  public set posts(newPosts: Post[]) {
+    this._posts = newPosts;
+  }
 }
